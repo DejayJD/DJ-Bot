5e1a78b12f123720aaf3f3e8a52b670d32792c30
EC2 Default User <ec2-user@ip-172-31-35-214.us-west-2.compute.internal>
2018-10-16 18:51:27 +0000

    Made logging use my timezone

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-
 //Import environment variables
 require('./environmentVars.js');
 require('./dist/services/ServiceManager.js');
-require('log-timestamp')(function() { return new Date().toLocaleString() + ' --- %s' })
+require('log-timestamp')(function() {
+	let currentTime = new Date();
+	currentTime.setHours(currentTime.getHours() - 5);
+	return currentTime.toLocaleString() + ' --- %s' 
+})
 
 //Setup a new application object
 //If ever needing massive performance, this could be separated
